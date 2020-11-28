8a528175c29a2bd1f42c0ec620f75210c015e0c0
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-28 15:17:11 +0300

    Create an object array for the design list

---
+++ pages/designList.js
@@ -0,0 +1,119 @@
+import frontEndAssessment from "./assets/fea.png";
+import frontEndFinal from "./assets/front-end-final.png";
+import gameLanding from "./assets/game-landing-page.png";
+import interiorConsultant from "./assets/interior-consultant.png";
+import myTeamPage from "./assets/my-team-page.png";
+import ghibliMovieList from "./assets/ghibli-movie-list.png";
+import birthdayApp from "./assets/birthday-app.png";
+import teaTimeTopic from "./assets/tea-time-topic.png";
+import randomQuote from "./assets/random-quote.png";
+import inputComponent from "./assets/input-component.png";
+import buttonComponent from "./assets/button-component.png";
+import countryQuiz from "./assets/country-quiz.png";
+import hitParade from "./assets/hit-parade.png";
+export default [
+    {
+        title: "Sports App",
+        url: "https://vakodrazan.github.io/front-end-assessment/",
+        code : "https://github.com/vakodrazan/front-end-assessment",
+        picture: frontEndAssessment,
+        id: 1604657565372,
+        description: "In this project, I work with HTML and CSS to create a responsive page. This is for practising the use of element.",
+    },
+    {
+        title: "Photograph",
+        url: "https://vakodrazan.github.io/front-end-finals/",
+        code: "https://github.com/vakodrazan/front-end-finals",
+        picture: frontEndFinal,
+        id: 1604657574337,
+        description: "In this project, I work with HTML and CSS to create a responsive page. This is for practising the use of element.",
+    },
+    {
+        title: "Game landing page",
+        url: "https://vakodrazan.github.io/game-landing-page/",
+        code: "https://github.com/vakodrazan/game-landing-page", 
+        picture: gameLanding,
+        id: 1604657595703,
+        description: "In this project, I work with HTML and CSS to create a responsive page. This is for practising the use of element.",
+    },
+    {
+        title: "Interior Consultant",
+        url: "https://interior-consultants.netlify.app/",
+        code: "https://github.com/vakodrazan/interior-consultant",
+        picture: interiorConsultant,
+        id: 1604657608163,
+        description: "In this project, I work with HTML and SASS to create a responsive page.",
+    },
+    {
+        title: "My Team Page",
+        url: "https://my-team-page-design.netlify.app/",
+        code: "https://github.com/vakodrazan/my_team_pages",
+        picture: myTeamPage,
+        id: 1604657622213,
+        description: "In this project, I work with HTML and SASS to create a responsive page.",
+    },
+    {
+        title: "Ghibli Movies",
+        url: "https://ghibli-movies-list.netlify.app/",
+        code: "https://github.com/vakodrazan/WKP9",
+        picture: ghibliMovieList,
+        id: 1604657629911,
+        description: "In this project, I work with react it's all about showing the list of Movies that is fetched from an API",
+    },
+    {
+        title: "Birthday APP",
+        url: "https://list-of-birthday-app.netlify.app/",
+        code: "https://github.com/vakodrazan/birthday-app",
+        picture: birthdayApp,
+        id: 1604657646561,
+        description: "In this project, I work with Vanilla Javascript. It is all about, adding a new item to list, deleting and editing",
+    },
+    {
+        title: "Tea Time Topic",
+        url: "https://react-tea-time-topic.netlify.app/",
+        code: "https://github.com/vakodrazan/react-tea-time-topic",
+        picture: teaTimeTopic,
+        id: 1604657656221,
+        description: "In this project, I work with React. It is all about showing the list and separating the list that has been archived and the ones which not.",
+    },
+    {
+        title: "Random Quotes Generator",
+        url: "https://random-quotes-generator-react.netlify.app/",
+        code: "https://github.com/vakodrazan/react-random-quote-generator",
+        picture: randomQuote,
+        id: 1604657667989,
+        description: "In this project, I work with React. It's all about fetching the data from an API and shpw the quotes randomly by clicking the button. Going to the list of quotes that are written by the author that has been clicked.",
+    },
+    {
+        title: "Input Component",
+        url: "https://input-components.netlify.app/",
+        code: "https://github.com/vakodrazan/input-component",
+        picture: inputComponent,
+        id: 1604657677246,
+        description: "In this project, I work with React. It is all about reusable components and a page displaying all the states.",
+    },
+    {
+        title: "Button Component",
+        url: "https://buttons-component.netlify.app/",
+        code: "https://github.com/vakodrazan/react-button",
+        picture: buttonComponent,
+        id: 1604657686375,
+        description: "In this project, I work with React. It is all about reusable components and a page displaying all the states.",
+    },
+    {
+        title: "Country Quiz",
+        url: "https://country-quiz-noeline.netlify.app/",
+        code: "https://github.com/vakodrazan/country-quiz",
+        picture: countryQuiz,
+        id: 1604657686675,
+        description: "In this project, I work with React. It is all about fetching data from a API that is not exactly the real API but still go deeper. It showing the question randomly with the choice of answer and must be included the right answers.",
+    },
+    {
+        title: "Hit Parade",
+        url: "https://hit-parade-react-noeline.netlify.app/",
+        code: "https://github.com/vakodrazan/hit-parade-react",
+        picture: hitParade,
+        id: 1604657686925,
+        description: "In this project, I work with React. It's all about music. You can favourite any music you like that is in the list also, you can add a new one.",
+    },
+]
